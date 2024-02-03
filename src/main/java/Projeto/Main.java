67370914b47cc558e4753c075c364d7179e6ec8f
Douglas-Dorug <douglasx30@gmail.com>
2024-02-02 21:35:15 -0300

    Pequeno ajuste na main

--- src/main/java/Projeto/Main.java
+++ src/main/java/Projeto/Main.java
@@ -1,44 +1,18 @@
 package Projeto;
 
-import Projeto.Entidades.Cliente;
-import Projeto.Entidades.Gerente;
-import Projeto.Repositorio.ClienteRepositorio;
-import Projeto.Repositorio.GerenteRepositorio;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import Projeto.Entidades.Conta;
-import Projeto.Enums.TiposConta;
-import Projeto.Repositorio.ContaRepositorio;
+import Projeto.Front.TelaLogin;
 
-import java.time.LocalDate;
-import java.util.Date;
-import org.hibernate.Transaction;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        
-        
-        //gerar tabelas
 
-        SessionFactory sessionFactory = new Configuration().configure("persistence.xml").buildSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TelaLogin().setVisible(true);
+            }
+        });
 
-        // Realize operações no banco de dados usando a sessão
-
-        transaction.commit();
-        session.close();
-
-        
-        
-        
-        
-        
-        
-        
-        
-
-         
     }
 }
